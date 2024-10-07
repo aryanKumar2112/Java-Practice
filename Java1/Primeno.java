ea4a3b8015fa65424d548b29c45a89153d7ee676
Aryan <aryankumar211202@gmail.com>
2024-10-07 22:17:52 +0530

    Prime no program

--- Java1/Primeno.java
+++ Java1/Primeno.java
@@ -4,7 +4,6 @@ public class Primeno {
     public static boolean checkPrime(int n){
         for(int i=2;i<n;i++){
             if(n%i==0){
-
                 return false;
             }
         }
